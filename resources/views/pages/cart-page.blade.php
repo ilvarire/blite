5259c68cf9fd460a4ede1064bd1ed95347a60821
Your Name <you@example.com>
2025-11-16 16:24:25 -0800

    h-a

--- resources/views/pages/cart-page.blade.php
+++ resources/views/pages/cart-page.blade.php
@@ -16,9 +16,8 @@
                     @forelse($moreFoods as $food)
                         <div
                             class="dz-shop-card style-1 flex border border-[#0000001a] rounded-[10px] mb-5 overflow-hidden duration-500 hover:border-transparent hover:shadow-[0px_15px_55px_rgba(34,34,34,0.15)] relative">
-                            <div class="dz-media w-[100px] min-w-[100px] overflow-hidden">
-                                <img src="{{ asset('storage/' . $food->image_url) }}" alt="/"
-                                    class="h-auto overflow-hidden">
+                            <div class="dz-media w-[100px] h-auto min-w-[100px] overflow-hidden">
+                                <img src="{{ asset('storage/' . $food->image_url) }}" alt="/" class="h-auto">
                             </div>
                             <div class="dz-content sm:p-5 p-2 flex flex-col w-full">
                                 <div class="dz-head mb-4 flex items-center justify-between">
